dbb3ee756c78568a653c834d12320cd68d62d892
Kylzo <torralba.f@outlook.fr>
2022-06-22 17:01:34 +0200

    script index et test

--- front/js/script.js
+++ front/js/script.js
@@ -1,31 +1,29 @@
-let article = `
-<a href="product.html?id=107fb5b75607497b96722bda5b504926">
-          <article>
-            <img src="back/images/kanap01.jpeg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-            <h3 class="productName">Kanap Sinopé</h3>
-            <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada
-              risus sapien gravida nulla nisl arcu.</p>
-              <p id="products"></p>
-          </article>
-        </a>
+const Url = "http://localhost:3000/api/products/";
+let cardsFetch = function () {
+  fetch(Url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-`
+      let productSection = document.getElementById("items");
 
-el = document.getElementById ("items")
-console.log ("123",el)
-
-el.innerHTML = article
-
-let url = `http://localhost:3000/api/products`
-fetch (url)
-.then(function(response){
-    console.log ("response",response)
-    return response.json()
-})
-.then(function(articles){
-    console.log ("json",articles)    
-})
-.catch(function(err){
-    console.log ("Une erreur est survenue", err)
-})
-console.log ("456")
+      for (i = 0; i < data.length; i++) {
+        const productCard = `
+          <a href="product.html?id=${data[i]._id}">
+            <article>
+              <img
+                src="${data[i].imageUrl}"
+                alt="${data[i].altTxt}"
+              />
+              <h3 class="productName">${data[i].name}</h3>
+              <p class="productDescription">
+                ${data[i].description}
+              </p>
+            </article>
+          </a>
+        `;
+        productSection.innerHTML += productCard;
+      }
+    });
+};
+cardsFetch();
